e0b11cd00abb1840b6176e2afa92eef75a085272
Cyenoch <1759761439@qq.com>
2021-06-02 08:53:41 +0800

    feat(DataList): Custom fetch data event

--- Controls/DataList.xaml.cs
+++ Controls/DataList.xaml.cs
@@ -1,13 +1,17 @@
 ﻿using Coolapk_UWP.Models;
 using Coolapk_UWP.Other;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-namespace Coolapk_UWP.Controls {
-    public sealed partial class DataList : UserControl, INotifyPropertyChanged {
+namespace Coolapk_UWP.Controls
+{
+    public sealed partial class DataList : UserControl, INotifyPropertyChanged
+    {
         // 对于GetDataList，需要Title参数
         public static readonly DependencyProperty titleProperty = DependencyProperty.Register(
             "Title",
@@ -29,18 +33,29 @@ namespace Coolapk_UWP.Controls {
             typeof(DataList),
             new PropertyMetadata(false)
         );
+        public static readonly DependencyProperty customMode = DependencyProperty.Register(
+             "CustomMode",
+             typeof(bool),
+             typeof(DataList),
+             new PropertyMetadata(false)
+         );
 
-        public string Title {
+        public event Func<LoadMoreItemsAsyncFuncConfig, Task<ICollection<Models.Entity>>> CustomFetchDataEvent;
+
+        public string Title
+        {
             get { return GetValue(titleProperty)?.ToString(); }
             set { SetValue(titleProperty, value); }
         }
 
-        public string Url {
+        public string Url
+        {
             get { return GetValue(urlProperty)?.ToString(); }
             set { SetValue(urlProperty, value); }
         }
 
-        public bool TouTiaoMode {
+        public bool TouTiaoMode
+        {
             get { return bool.Parse(GetValue(toutiaoMode)?.ToString() ?? "False"); }
             set { SetValue(toutiaoMode, value); }
         }
@@ -49,12 +64,20 @@ namespace Coolapk_UWP.Controls {
 
         public uint installTime = (uint)AppUtil.DateToTimeStamp(DateTime.Now);
 
-        public DataList() {
+        public DataList()
+        {
             this.InitializeComponent();
             //DispatcherPriority.DataBind = 8
-            _ = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
-                if (TouTiaoMode) { // 头条模式 也就是首页 URL https://api.coolapk.com/v6/main/indexV8?page=&firstLaunch=&installTime=&lastItem=
-                    Set(ref Entities, new IncrementalLoadingEntityCollection<Entity>(async config => {
+            _ = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (CustomFetchDataEvent != null)
+                {
+                    Set(ref Entities, new IncrementalLoadingEntityCollection<Entity>(CustomFetchDataEvent), "Entities");
+                }
+                else if (TouTiaoMode)
+                { // 头条模式 也就是首页 URL https://api.coolapk.com/v6/main/indexV8?page=&firstLaunch=&installTime=&lastItem=
+                    Set(ref Entities, new IncrementalLoadingEntityCollection<Entity>(async config =>
+                    {
                         var resp = await App.AppViewModel.CoolapkApis.GetIndexV8(
                             installTime,
                             config.Page,
@@ -62,8 +85,10 @@ namespace Coolapk_UWP.Controls {
                         );
                         return resp.Data;
                     }), "Entities");
-                } else
-                    Set(ref Entities, new IncrementalLoadingEntityCollection<Entity>(async config => {
+                }
+                else
+                    Set(ref Entities, new IncrementalLoadingEntityCollection<Entity>(async config =>
+                    {
                         var resp = await App.AppViewModel.CoolapkApis.GetDataList(
                             Url,
                             Title,
@@ -77,8 +102,10 @@ namespace Coolapk_UWP.Controls {
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void Set<T>(ref T storage, T value, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null) {
-            if (Equals(storage, value)) {
+        protected void Set<T>(ref T storage, T value, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
                 return;
             }
             storage = value;
@@ -86,13 +113,16 @@ namespace Coolapk_UWP.Controls {
         }
         public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-        private void EntityListView_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e) {
+        private void EntityListView_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
             // f*ck
         }
 
-        private void EntityListView_ItemClick(object sender, ItemClickEventArgs e) {
+        private void EntityListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
             var entity = e.ClickedItem;
-            if (entity is Feed) {
+            if (entity is Feed)
+            {
                 // feed类留给相应item自行处理
                 //var feed = entity as Feed;
                 //App.AppViewModel.HomeContentFrame.Navigate(typeof(Coolapk_UWP.Pages.FeedDetail), feed.EntityID);
