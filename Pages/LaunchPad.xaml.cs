9cd4939ae69028961e0b771c936f5f3f32efa34c
Cyenoch <1759761439@qq.com>
2021-06-02 09:57:38 +0800

    feat(LaunchPad): add join group link

--- Pages/LaunchPad.xaml.cs
+++ Pages/LaunchPad.xaml.cs
@@ -16,26 +16,37 @@ using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
-namespace Coolapk_UWP.Pages {
-    public sealed partial class LaunchPad : Page {
-        public AppViewModel ViewModel {
+namespace Coolapk_UWP.Pages
+{
+    public sealed partial class LaunchPad : Page
+    {
+        public AppViewModel ViewModel
+        {
             get { return App.AppViewModel; }
         }
-        public LaunchPad() {
+        public LaunchPad()
+        {
             this.InitializeComponent();
             DataContext = ViewModel;
         }
 
-        private void LaunchPadListView_ItemClick(object sender, ItemClickEventArgs e) {
-            switch((e.ClickedItem as FrameworkElement).Name) {
+        private void LaunchPadListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            switch ((e.ClickedItem as FrameworkElement).Name)
+            {
                 case "GoToLogin":
                     App.AppViewModel.HomeContentFrame.Navigate(typeof(Login));
                     break;
                 case "GoToGitHub":
+                    var uri = new Uri(@"https://github.com/Cyenoch/Coolapk-UWP");
+                    Windows.System.Launcher.LaunchUriAsync(uri);
                     break;
                 case "GoToUserProfile":
                     break;
+                case "FollowDeveloper":
+                    break;
                 case "GoToQQGroup":
+                    Windows.System.Launcher.LaunchUriAsync(new Uri(@"https://jq.qq.com/?_wv=1027&k=0Uvdqt1d"));
                     break;
             }
         }
